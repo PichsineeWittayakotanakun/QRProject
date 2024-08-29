2b8de871deaea808ef61d67150fce4ca25f6931f
PichsineeWitt <pichsinee.wittayakotanakun@bangkokbank.com>
2024-08-29 15:51:36 +0700

    update save to camera rolls

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ features for create QR code and decode QR code from in app camera or pick image 
 ## What we have
 1. Preview QR code
 2. Generate QR code
-3. Save QR code to camera rolls //still working
+3. Save QR code to camera rolls
 4. Scan QR code from in app camera
 5. Read image from camera rolls and decode QR code value
 
--- src/features/GenerateQRScreen.tsx
+++ src/features/GenerateQRScreen.tsx
@@ -6,15 +6,99 @@ import {
   TouchableOpacity,
   ScrollView,
   View,
+  Platform,
+  Alert,
+  Linking,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import colors from '../shared/colors';
 import QRCode from 'react-native-qrcode-svg';
+import {check, PERMISSIONS, request} from 'react-native-permissions';
+import ViewShot from 'react-native-view-shot';
+import {CameraRoll} from '@react-native-camera-roll/camera-roll';
 
 const GenerateQRScreen = () => {
   const [value, setValue] = useState<string>('Hello world!');
   const [qrValue, setQrValue] = useState<string>(value);
   const buttonDisable = value.length === 0;
+  const viewShotRef = useRef<ViewShot>(null);
+  const [permissionToggle, setPermissionToggle] = useState<number>(0);
+  const noAccessAlert = () =>
+    Alert.alert(
+      'No camera access',
+      'Please go to the device settings page and allow app to access save to gallery.',
+      [
+        {
+          text: 'OK',
+          onPress: () => {
+            Linking.openSettings();
+          },
+        },
+      ],
+    );
+  const saveToAlbumSuccess = () =>
+    Alert.alert('Success!', 'Save to album success', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  const saveToAlbumFail = () =>
+    Alert.alert('Fail!', 'Save to album fail', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+
+  const checkCameraPermission = async () => {
+    const permissionType =
+      Platform.OS === 'android'
+        ? PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE
+        : PERMISSIONS.IOS.PHOTO_LIBRARY;
+    const result = await check(permissionType);
+    if (result === 'denied') {
+      const reqResult = await request(permissionType);
+      if (reqResult === 'granted') {
+        setPermissionToggle(permissionToggle + 1);
+      } else if (reqResult === 'blocked' || reqResult === 'denied') {
+        noAccessAlert();
+      }
+    } else if (result === 'blocked') {
+      noAccessAlert();
+    } else if (result === 'granted') {
+      captureQRCode();
+    }
+  };
+
+  useEffect(() => {
+    if (permissionToggle > 0) {
+      checkCameraPermission();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [permissionToggle]);
+
+  const captureQRCode = () => {
+    setTimeout(() => {
+      try {
+        if (viewShotRef.current && viewShotRef.current.capture) {
+          viewShotRef.current
+            .capture()
+            .then(async (uri: string) => {
+              await CameraRoll.saveAsset(uri, {type: 'photo'});
+            })
+            .then(() => {
+              saveToAlbumSuccess();
+            });
+        } else {
+          saveToAlbumFail();
+        }
+      } catch (error) {
+        saveToAlbumFail();
+      }
+    }, 500);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView bounces={false} style={styles.scrollviewContainer}>
@@ -38,10 +122,19 @@ const GenerateQRScreen = () => {
               <Text style={styles.buttonText}>Generate</Text>
             </TouchableOpacity>
           )}
-          <View style={styles.qrContainer}>
-            <QRCode value={qrValue} ecl="H" quietZone={10} size={200} />
-          </View>
+          <ViewShot ref={viewShotRef} options={{format: 'png', quality: 0.9}}>
+            <View style={styles.qrContainer}>
+              <QRCode value={qrValue} ecl="H" quietZone={10} size={200} />
+            </View>
+          </ViewShot>
           <Text style={styles.qrValueText}>{`QR value : ${qrValue}`}</Text>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => {
+              checkCameraPermission();
+            }}>
+            <Text style={styles.buttonText}>Save QR Image</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
